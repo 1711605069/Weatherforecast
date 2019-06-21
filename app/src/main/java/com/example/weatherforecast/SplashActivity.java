c3c5a2c698dcf76ddb545dca0e338acd1e3ae918
ITVictor1997 <1402523791@qq.com>
2019-06-21 10:41:33 +0800

    实现了欢迎界面的跳转

--- app/src/main/java/com/example/weatherforecast/SplashActivity.java
+++ app/src/main/java/com/example/weatherforecast/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.example.weatherforecast;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //3秒后跳转到MainActivity.java
+        //定时器和任务
+        Timer timer = new Timer();//定时器
+        TimerTask task = new TimerTask() {//定义任务，实现Runn接口
+            @Override
+            public void run() {//重定run（）方法
+                Intent intent = new Intent(getApplicationContext(), WeatherActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(task, 3000);
     }
 }
